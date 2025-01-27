dc9c742192264b8f8680d5a1e55a91c4fddf5b1c
Sarah Walker <sarahsweetpie6@gmail.com>
2025-01-27 09:04:42 -0700

    added array

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿using System;
 
 Console.WriteLine("Welcome to TicTacToe!");
+
+string[] gameBoard = {"", "", "",
+                      "", "", "",
+                      "", "", "" }
+
+
 
