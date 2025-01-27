1682f095ddc6942fcc8e274ca64aada7f211bd1e
Channing Paxman <channing.paxman@gmail.com>
2025-01-27 09:33:25 -0700

    new tools

--- TicTacTools.cs
+++ TicTacTools.cs
@@ -2,5 +2,31 @@ namespace tictactoe;
 
 public class TicTacTools
 {
-    
+    public string[] UpdateGameBoard(string[] gameBoard, int spot, bool turn)
+    {
+        if (spot < 1 || spot > gameBoard.Length)
+        {
+            Console.WriteLine($"Invalid spot. Choose a number between 1 and {gameBoard.Length}.");
+            return gameBoard;
+        }
+        
+        if (gameBoard[spot - 1] != "")
+        {
+            Console.WriteLine("Spot already taken. Choose a different one");
+            return gameBoard;
+        }
+        else
+        {
+            if (turn)
+            {
+                gameBoard[spot - 1] = "X";
+            }
+            else
+            {
+                gameBoard[spot - 1] = "O";
+            }
+        }
+        return gameBoard;
+
+    }
 }
