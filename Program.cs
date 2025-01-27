73fbe8d99174c180f8b9cebd582cc2630e0db3fa
shicr <shicr@byu.edu>
2025-01-27 09:12:17 -0700

    changes

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 
+// • Welcome the user to the game
 Console.WriteLine("Welcome to TicTacToe!");
 
+
+// • Create a game board array to store the players’ choices
 string[] gameBoard = {"1", "2", "3",
                       "4", "5", "6",
                       "7", "8", "9" }
