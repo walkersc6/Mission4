e4866d4979a40395a0930bc2b8cfc45fdfd3509e
Sarah Walker <sarahsweetpie6@gmail.com>
2025-01-27 09:25:48 -0700

    added code

--- Program.cs
+++ Program.cs
@@ -12,5 +12,33 @@ string[] gameBoard =
     "7", "8", "9"
 };
 
+bool gameWon = false;
+int plays = 0;
+
+//create while loop that iterates while there is no winner or number of turns is 9
+while (gameWon != false || plays < 9)
+{
+
+    // player1
+
+    //player2
+
+    plays = plays + 2;
+
+
+    //Ask each player in turn for their choice
+
+
+    //update the game board array
+
+
+    //call print board method
+
+
+    //Check for winner method
+
+}
+
+
 
 
