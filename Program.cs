1beb099ae936e91d8ddc069cfa66c695d58bf7ae
Sarah Walker <sarahsweetpie6@gmail.com>
2025-01-27 09:10:18 -0700

    changed array

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 
 Console.WriteLine("Welcome to TicTacToe!");
 
-string[] gameBoard = {"", "", "",
-                      "", "", "",
-                      "", "", "" }
+string[] gameBoard = {"1", "2", "3",
+                      "4", "5", "6",
+                      "7", "8", "9" }
 
 
 
