3f2ce2544310d4db23413fc9885bdf7df0dd02b3
Channing Paxman <channing.paxman@gmail.com>
2025-01-27 10:22:00 -0700

    updated TTT

--- TicTacTools.cs
+++ TicTacTools.cs
@@ -4,29 +4,20 @@ public class TicTacTools
 {
     public string[] updateGameBoard(string[] gameBoard, int spot, bool turn)
     {
-        if (spot < 1 || spot > gameBoard.Length)
+        while (spot < 1 || spot > gameBoard.Length || gameBoard[spot - 1] != spot.ToString())
         {
             Console.WriteLine($"Invalid spot. Choose a number between 1 and {gameBoard.Length}.");
-            return gameBoard;
+            spot = int.Parse(Console.ReadLine());
         }
-        
-        if (gameBoard[spot - 1] != spot.ToString())
+
+        if (turn)
         {
-            Console.WriteLine("Spot already taken. Choose a different one");
-            return gameBoard;
+            gameBoard[spot - 1] = "X";
         }
         else
         {
-            if (turn)
-            {
-                gameBoard[spot - 1] = "X";
-            }
-            else
-            {
-                gameBoard[spot - 1] = "O";
-            }
+            gameBoard[spot - 1] = "O";
         }
         return gameBoard;
-
     }
 }
